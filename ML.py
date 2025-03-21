efb2d3d04e91a113d1360c28046fccdf2c34731e
Renaud <132277087+renaudsrr@users.noreply.github.com>
2025-03-21 21:45:22 +0100

    Update ML.py

--- ML.py
+++ ML.py
@@ -8,7 +8,7 @@ History of modification
 12.03.2025 : Creation of the script. Import data 
 13.03.2025 : Some test of RF
 18.03.2025 : Contnuation and basics stuff of XGboost, SHAP
-20.03.2025 : Huge amelioration implementation of different choice of modelisation
+
 """
 
 # =============================================================================
@@ -26,8 +26,9 @@ from statsmodels.stats.outliers_influence import OLSInfluence
 import xgboost as xgb
 import shap
 import optuna
+optuna.logging.set_verbosity(optuna.logging.WARNING)
 
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, cross_val_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.inspection import PartialDependenceDisplay
 from sklearn.preprocessing import StandardScaler
@@ -38,19 +39,24 @@ from sklearn.preprocessing import StandardScaler
 # =======================================================================================================================
 # =======================================================================================================================
 
+# Choix de paramétrage de modélisation
 compare_model = input("(Vide = N) // Comparaison 3 model ? (Y/N)") or "N"
 
 if compare_model =="Y":
     refaire_optimisation = input("(Vide = N) // Refaire l'optimisation hyperparams ? (Y/N) ") or "N"
     if refaire_optimisation =="Y":
-        n_trials_opt_Rd = int(input("(Vide = 100) // Nb essais optimisation hyperparam Randomised : ")) or 100
-        n_trials_opt_Op = int(input("(Vide = 80) // Nb essais optimisation hyperparam optuna : ")) or 80
+        input_rd = input("(Vide = 100) // Nb essais optimisation hyperparam Randomised : ")
+        n_trials_opt_Rd = int(input_rd) if input_rd.strip() != "" else 100
+
+        input_op = input("(Vide = 80) // Nb essais optimisation hyperparam optuna : ")
+        n_trials_opt_Op = int(input_op) if input_op.strip() != "" else 80
 
 choix_div = input("(Vide = H) // Choix indice de diversite ? (rich_genus, H, 1-D, J) ") or "H"
 
-normalisation_VA_expl = input("(Vide = N) // Normaliser les VA explicatives ? (Y/N) ") or "N"
+suppr_ind_influents = input("(Vide = N) // Supprimer individus trop influents ? (Y/N) ")
 
-test_size = 0.3
+# % données utilisées pour X_test et y_test
+test_size = 0.1
 
 print("\n")
 
@@ -58,17 +64,46 @@ print("\n")
 # Data
 # =============================================================================
 
-data_phyto = pd.read_csv("Data/new_csv/data_phyto.csv") 
-data_zoo = pd.read_csv("Data/new_csv/data_zoo.csv")
+data_python = pd.read_csv("Data/new_csv/data_python.csv",index_col=0)
+VA_expl = data_python.drop(columns=["region","lake","type_prof","rich_genus","H","1-D","J"])
 
-# DATA AVEC : 1ère VA Reponse = 4 métriques de diversité phyto
-summary_data_phyto = pd.read_csv("Data/new_csv/summary_data_phyto.csv") 
-#  DATA AVEC : 2ème VA Reponse = 4 métriques de diversité zoo
-summary_data_zoo = pd.read_csv("Data/new_csv/summary_data_zoo.csv") 
+# Traitement des NaN (Provisioire) ============================================
 
-# DATA AVEC : all VA Explicatives
-VA_expl = pd.read_csv("Data/new_csv/VA_expl.csv")
-VA_expl = VA_expl.drop(columns=["Sample","region","lake","type_prof","doy","yr"])
+VA_expl["biomass_tot_zoo"] = VA_expl["biomass_tot_zoo"].fillna(VA_expl["biomass_tot_zoo"].median())
+VA_expl["sech.m"] = VA_expl["sech.m"].fillna(VA_expl["sech.m"].median())
+
+VA_expl.to_csv("Data/new_csv/VA_expl_python.csv")
+
+# =============================================================================
+# Extraction data pour ML
+# =============================================================================
+
+y = data_python[choix_div] # Variable réponse
+# Séparation avec jeu learning et test, ajout random_state = 42 si besoin pour cadrer résultats predi
+X_train, X_test, y_train, y_test = train_test_split(VA_expl, y, test_size=test_size,random_state = 42) 
+
+print("var train : ", np.var(y_train), " // var test : ", np.var(y_test))
+print("\n")
+# =============================================================================
+# Supprimer individus trop influents
+# =============================================================================
+
+if suppr_ind_influents == "Y":
+    X_train_const = sm.add_constant(X_train)
+    model = sm.OLS(y_train, X_train_const).fit()
+
+    # Calcul des distances de Cook
+    influence = model.get_influence()
+    cooks_d = influence.cooks_distance[0]
+
+    n = len(X_train)
+    threshold = 4 / n  # Seuil couramment utilisé
+
+    influential_points = np.where(cooks_d > threshold)[0]
+    influential_indices = X_train.index[influential_points]
+    print("sample influents : ", list(influential_indices))
+    X_train = X_train.drop(index=influential_indices)
+    y_train = y_train.drop(index=influential_indices)
 
 # =============================================================================
 # Exploration data
@@ -76,58 +111,13 @@ VA_expl = VA_expl.drop(columns=["Sample","region","lake","type_prof","doy","yr"]
 
 plt.figure(figsize=(10, 8))
 
-plt.subplot(2, 2, 1)
-plt.scatter(summary_data_phyto["prev_Mixo"], summary_data_phyto["rich_genus"], alpha=0.7, color="blue")
-plt.xlabel("prev_Mixo")
-plt.ylabel("rich_genus")
+plt.scatter(X_train["prev_Mixo"],y_train,color="green",alpha=0.7,label="train")
+plt.scatter(X_test["prev_Mixo"],y_test,color="green",alpha=0.3,label="test")
 
-plt.subplot(2, 2, 2)
-plt.scatter(summary_data_phyto["prev_Mixo"], summary_data_phyto["H"], alpha=0.7, color="green")
-plt.xlabel("prev_Mixo")
-plt.ylabel("H")
-
-plt.subplot(2, 2, 3)
-plt.scatter(summary_data_phyto["prev_Mixo"], summary_data_phyto["1-D"], alpha=0.7, color="red")
-plt.xlabel("prev_Mixo")
-plt.ylabel("1-D")
-
-plt.subplot(2, 2, 4)
-plt.scatter(summary_data_phyto["prev_Mixo"], summary_data_phyto["J"], alpha=0.7, color="purple")
-plt.xlabel("prev_Mixo")
-plt.ylabel("J")
-
+plt.legend()
 plt.tight_layout()
 plt.show()
 
-# =============================================================================
-# Traitement des NaN (Provisioire)
-# =============================================================================
-
-VA_expl["biomass_tot_zoo"] = VA_expl["biomass_tot_zoo"].fillna(VA_expl["biomass_tot_zoo"].median())
-VA_expl["sech.m"] = VA_expl["sech.m"].fillna(VA_expl["sech.m"].median())
-
-VA_expl.to_csv("Data/new_csv/VA_expl_python.csv", index=False)
-
-# =============================================================================
-# Extraction data pour ML
-# =============================================================================
-
-y = summary_data_phyto[choix_div] # Variable réponse
-# Séparation avec jeu learning et test, ajout random_state = 42 si besoin pour cadrer résultats predi
-X_train, X_test, y_train, y_test = train_test_split(VA_expl, y, test_size=test_size) 
-
-# =============================================================================
-# Normalisation
-# =============================================================================
-
-if normalisation_VA_expl =="Y":
-    scaler = StandardScaler()
-
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
-    X_train = pd.DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index)
-    X_test = pd.DataFrame(X_test_scaled, columns=X_test.columns, index=X_test.index)
 
 # =============================================================================
 # Optimisation hyperparamètres
@@ -135,44 +125,45 @@ if normalisation_VA_expl =="Y":
 
 if compare_model == "Y" and refaire_optimisation == "Y":
 
+    print("opti Rd en cours...")
+
     # Randomise ==================================================================
 
     param_dist = {
         'n_estimators': [50, 100, 300, 500, 1000, 2000],
-        'max_depth': [3, 6, 9, 12, 15, 20],
-        'learning_rate': uniform(0.001, 0.3),  
-        'subsample': uniform(0.5, 0.5),  
+        # 'max_depth': [3, 6, 9, 12, 15, 20],
+        # 'learning_rate': uniform(0.001, 0.3),  
+        # 'subsample': uniform(0.5, 0.5),  
         'colsample_bytree': uniform(0.5, 0.5),
-        'reg_lambda': uniform(0, 10),  
-        'reg_alpha': uniform(0, 5),  
-        'min_child_weight': [1, 3, 5, 7, 10],
-        'gamma': uniform(0, 5)
+        # 'reg_lambda': uniform(0, 10),  
+        # 'reg_alpha': uniform(0, 5),  
+        'gamma' : uniform(0,0.5)
     }
 
     xgb_model_Rd = xgb.XGBRegressor(random_state=42)
-    random_search = RandomizedSearchCV(xgb_model_Rd, param_dist, cv=5, scoring='r2', n_jobs=-1, n_iter=n_trials_opt_Rd, verbose=3)
+    random_search = RandomizedSearchCV(xgb_model_Rd, param_dist, cv=5, scoring='r2', n_jobs=-1, n_iter=n_trials_opt_Rd)
     random_search.fit(X_train, y_train)
 
     # Bayesien Optuna ==================================================================
 
+    print("opti Op en cours...")
+
     def objective(trial):
         params = {
-            'n_estimators': trial.suggest_int('n_estimators', 50, 3000),
-            'max_depth': trial.suggest_int('max_depth', 3, 20),
-            'learning_rate': trial.suggest_float('learning_rate', 0.001, 0.3, log=True),
-            'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+            'n_estimators': trial.suggest_int('n_estimators', 50, 2000),
+            # 'max_depth': trial.suggest_int('max_depth', 3, 10),
+            # 'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3, log=True),
+            # 'subsample': trial.suggest_float('subsample', 0.5, 1.0),
             'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),
-            'random_state': 42,
-            'reg_lambda': trial.suggest_float('reg_lambda', 0.0, 10.0),
-            'reg_alpha': trial.suggest_float('reg_alpha', 0.0, 5.0),
-            'booster': 'gbtree',
-            'objective': 'reg:squarederror'
+            # 'reg_lambda': trial.suggest_float('reg_lambda', 0.0, 1.0),
+            # 'reg_alpha': trial.suggest_float('reg_alpha', 0.0, 1.0),
+            'gamma' : trial.suggest_float('gamma',0.0,0.5)
         }
         
         xgb_model_Op = xgb.XGBRegressor(**params)
-        xgb_model_Op.fit(X_train, y_train)
-        preds = xgb_model_Op.predict(X_test)
-        return r2_score(y_test, preds)
+        
+        score = cross_val_score(xgb_model_Op, X_train, y_train, cv=5, scoring="r2",n_jobs=-1)
+        return score.mean()
 
     study = optuna.create_study(direction='maximize')
     study.optimize(objective, n_trials=n_trials_opt_Op)
@@ -181,45 +172,50 @@ if compare_model == "Y" and refaire_optimisation == "Y":
     hyperparams_Op = pd.DataFrame.from_dict(study.best_params, orient="index", columns=["Opti_Op"])
 
     all_optimisation = pd.merge(hyperparams_Rd, hyperparams_Op, left_index=True, right_index=True, how="outer")
-    ordre_hyperparams = ["learning_rate", "max_depth", "n_estimators", "colsample_bytree", "subsample", "reg_alpha", "reg_lambda", "min_child_weight", "gamma", "max_delta_step", "booster"]
+    ordre_hyperparams = ["learning_rate", "max_depth", "n_estimators", "colsample_bytree", "subsample", "reg_alpha", "reg_lambda", "gamma"]
     all_optimisation = all_optimisation.reindex(ordre_hyperparams)
 
     all_optimisation.to_csv(f"Data/new_csv/all_optimisation_{choix_div}.csv")
 
-    print("\n\n")
+    print("\n")
     print(f"Optimisation hyperparams pour y = {choix_div}")
     print(all_optimisation)
-    print("\n\n")
+    print(f"Meilleur R² Optuna : {study.best_value:.4f}")
+    print("\n")
 
 # =============================================================================
 # Creation model
 # =============================================================================
 
 xgb_model_1 = xgb.XGBRegressor(
-    random_state = 42,        # graine aleatoire pour reproductibilite
-    # grow_policy = "depthwise",
-    # importance_type = "gain",
+    random_state = 42,       # Graine aléatoire pour assurer la reproductibilité des résultats
 
-    eta = 0.01,
+    # grow_policy = "depthwise",   # Politique de croissance des arbres (par défaut : "depthwise", sinon "lossguide")
 
-    learning_rate = 0.1,    # Plus il est bas, plus le modèle apprend lentement mais en généralisant mieux => Shrinkage
-    max_depth = 6,           # Profondeur max des arbres => traux haut = overfitting
-    n_estimators = 800,     # nb arbres
+    # importance_type = "gain",   # Mesure l'importance des variables en fonction du gain apporté par chaque split
 
-    colsample_bytree = 0.8,  # Prend 80 % des colonnes = variables dans chaque arbre
-    subsample = 0.8,         # Prend 80 % des lignes du jeu learning pour chaque arbrew => evite overfitting
+    # learning_rate = 0.1,    # Taux d'apprentissage (plus bas = apprentissage lent mais meilleure généralisation, "shrinkage")
+    # max_depth = 15,           # Profondeur max des arbres (plus élevé = plus de complexité et risque d'overfitting)
+    n_estimators = 500000,      # Nombre total d'arbres dans l'ensemble (plus élevé = modèle plus complexe et plus lent)
 
-    reg_alpha = 0.05,  # L1 Regularization : LASSO : Favorise des coefficients proches de zero et peut eliminer certaines variables inutiles
-    reg_lambda = 0.5,  # L2 Regularization : RIDGE Reduit l'importance des coefficients pour eviter les valeurs extremes
-    
-    min_child_weight = 4,  # Permet de faire plus de splits, réduit le sous-ajustement
-    gamma = 0.2,  # Prune légèrement les splits inutiles, mais garde une bonne flexibilité
+    colsample_bytree = 0.8,   # Proportion des colonnes (features) utilisées pour chaque arbre (ex: 80% des variables)
+    # subsample = 0.8,          # Proportion des échantillons (lignes) utilisées pour chaque arbre (ex: 80% des lignes) 
+    #                          # Permet d'ajouter de la variance et réduit le risque d'overfitting
 
-    objective = "reg:squarederror",  # Objectif de régression classique
-    booster = "gbtree",  # Utilisation de l’approche en arbres
-    importance_type = "gain",  # Importance des features basée sur le gain
+    # reg_alpha = 0.0,         # Régularisation L1 (LASSO) : Encourage la réduction de certains coefficients à zéro (sparse model)
+    # reg_lambda = 0.0,         # Régularisation L2 (Ridge) : Réduit la magnitude des coefficients pour éviter les valeurs extrêmes
 
-    eval_metric = "auc"
+    # min_child_weight = 4,    # Nombre minimal d'échantillons requis dans un nœud pour qu'il puisse être divisé 
+                             # (plus haut = moins de splits, prévient l'overfitting)
+
+    gamma = 0.2,            # Seuil minimum de réduction de la perte pour effectuer un split 
+                             # (plus élevé = moins de splits inutiles)
+
+    # objective = "reg:squarederror",   # Fonction de coût pour un problème de régression classique (MSE)
+
+    # booster = "gbtree",      # Type de booster utilisé (gbtree = gradient boosting tree)
+
+    eval_metric = "rmse"    # Indicateur d'évaluation utilisé (par défaut)
     )
 
 if compare_model=="N":
@@ -239,15 +235,17 @@ if compare_model =="Y":
         # grow_policy = "depthwise",
         # importance_type = "gain",
 
-        learning_rate = all_optimisation.loc["learning_rate", "Opti_Rd"],    
-        max_depth = int(all_optimisation.loc["max_depth", "Opti_Rd"]),          
+        # learning_rate = all_optimisation.loc["learning_rate", "Opti_Rd"],    
+        # max_depth = int(all_optimisation.loc["max_depth", "Opti_Rd"]),          
         n_estimators = int(all_optimisation.loc["n_estimators", "Opti_Rd"]),    
 
         colsample_bytree = all_optimisation.loc["colsample_bytree", "Opti_Rd"],  
-        subsample = all_optimisation.loc["subsample", "Opti_Rd"],         
+        # subsample = all_optimisation.loc["subsample", "Opti_Rd"],         
 
-        reg_alpha = all_optimisation.loc["reg_alpha", "Opti_Rd"],  
-        reg_lambda = all_optimisation.loc["reg_lambda", "Opti_Rd"],
+        # reg_alpha = all_optimisation.loc["reg_alpha", "Opti_Rd"],  
+        # reg_lambda = all_optimisation.loc["reg_lambda", "Opti_Rd"],
+        
+        gamma = all_optimisation.loc["gamma", "Opti_Rd"],  
         )
 
     xgb_model_3 = xgb.XGBRegressor(
@@ -255,15 +253,17 @@ if compare_model =="Y":
         # grow_policy = "depthwise",
         # importance_type = "gain",
         
-        learning_rate = all_optimisation.loc["learning_rate", "Opti_Op"],    
-        max_depth =  int(all_optimisation.loc["max_depth", "Opti_Op"]),          
+        # learning_rate = all_optimisation.loc["learning_rate", "Opti_Op"],    
+        # max_depth =  int(all_optimisation.loc["max_depth", "Opti_Op"]),          
         n_estimators =  int(all_optimisation.loc["n_estimators", "Opti_Op"]),    
 
         colsample_bytree =  all_optimisation.loc["colsample_bytree", "Opti_Op"],  
-        subsample =  all_optimisation.loc["subsample", "Opti_Op"],         
+        # subsample =  all_optimisation.loc["subsample", "Opti_Op"],         
 
-        reg_alpha =  all_optimisation.loc["reg_alpha", "Opti_Op"],  
-        reg_lambda =  all_optimisation.loc["reg_lambda", "Opti_Op"],
+        # reg_alpha =  all_optimisation.loc["reg_alpha", "Opti_Op"],  
+        # reg_lambda =  all_optimisation.loc["reg_lambda", "Opti_Op"],
+        
+        gamma = all_optimisation.loc["gamma", "Opti_Op"],  
         )
                                              
 # =============================================================================
@@ -275,7 +275,7 @@ if compare_model=="Y":
     xgb_model_2.fit(X_train,y_train)
     xgb_model_3.fit(X_train,y_train)
 else:
-    xgb_model_1.fit(X_train,y_train)
+    xgb_model_1.fit(X_train,y_train, verbose=False)
 
 # =============================================================================
 # Prediction
@@ -342,18 +342,21 @@ print("\n")
 # Avec Score F = fréquence d’utilisation d’une variable comme critère de séparation dans les arbres de décision du model
 if compare_model =="Y":
     xgb.plot_importance(xgb_model_1,title = "model 1")
-    plt.show()
     xgb.plot_importance(xgb_model_2,title = "model 2")
-    plt.show()
     xgb.plot_importance(xgb_model_3,title = "model 3")
     plt.show()
 else :
     xgb.plot_importance(xgb_model_1,title = "model 1")
     plt.show()
 
+fi = pd.DataFrame(data=xgb_model_1.feature_importances_,
+    index=xgb_model_1.feature_names_in_, columns=["importance"]).sort_values("importance")
+
 # =============================================================================
 # SHAP (SHapley Additive Explanations) 
 # =============================================================================
+
+# Avec valeur SHAP = indique dans quelle mesure une variable influence la prédiction du modèle. (Shap positif => influence fit positif). couleur = valeut de la variable
 
 if compare_model =="Y":
     explainer_1 = shap.Explainer(xgb_model_1, X_train)
@@ -434,7 +437,7 @@ if compare_model =="Y":
     plt.hist(residus_3, bins=30, color="grey")
     
     plt.subplot(4,2,7)
-    X_train_const = sm.add_constant(X_train)  # Ajout d'une constante pour le modèle OLS
+    X_train_const = sm.add_constant(X_train) 
     model = sm.OLS(y_train, X_train_const).fit()
     influence = OLSInfluence(model)
     cooks_d = influence.cooks_distance[0]
@@ -451,31 +454,3 @@ else:
     plt.scatter(y_pred_1, residus_1, alpha=0.6)
     plt.axhline(y=0, color='r', linestyle='-')
     plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
